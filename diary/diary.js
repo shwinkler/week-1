7ae3a7f68641945b80db51dd205306b670f9fb81
jafow <jared.a.fowler@gmail.com>
2016-08-01 16:17:25 -0700

    create diary.js streaming stdin to file

--- diary/diary.js
+++ diary/diary.js
@@ -2,11 +2,12 @@
 const fs = require('fs')
 
 // check to see if your diary.txt file is there. if not make it
-if (!fs.stat('./diary.txt')) {
+if (!fs.stat('./diary/diary.txt')) {
   fs.writeFile('./diary/diary.txt', '', (err) => {
     if (err) throw err
     console.log('created a file')
   })
 }
 
+// pipe command line into diary.txt file
 process.stdin.pipe(fs.createWriteStream('diary.txt'))
