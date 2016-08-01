2ddd9a79a11d8f322161aa395e4ae21a253d1022
jafow <jared.a.fowler@gmail.com>
2016-08-01 00:35:53 -0700

    adds loop challenges

---
+++ loops.js
@@ -0,0 +1,22 @@
+/* write a function `countUp` that takes a number `N` and returns a string from 1 - N,
+ * separated by commas. The last number should NOT be followed by a comma.
+ * For example: countUp(4) // => '1, 2, 3, 4'
+ */
+var countUp = function (N) {
+  // code here
+}
+
+
+/* write a function `fizzBuzz` that takes a number `N` and returns an array of
+ * numbers from 1 - N. For multiples of 3, push 'fizz', for multiples of 5 push
+ * 'buzz', for multiples of 3 AND 5 push 'fizzbuzz'.
+ */
+var fizzBuzz = function (N) {
+  // code here
+}
+
+
+module.exports = {
+  countUp: countUp,
+  fizzBuzz: fizzBuzz
+}
