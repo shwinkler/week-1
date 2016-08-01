50e8a633dd035b00ede5d629e5680a5175504164
jafow <jared.a.fowler@gmail.com>
2016-07-31 23:04:42 -0700

    creates repeater fn and tests

--- test/test.js
+++ test/test.js
@@ -2,11 +2,25 @@ const test = require('tape')
 const variables = require('./../variables')
 
 test('Test variables challenge', (t) => {
-  t.plan(2)
+  t.plan(4)
   var apple = variables.apple
+  var fruit = variables.fruit
 
-  t.equal(apple, 'apple', 'Apple var should be set to "apple"')
+  t.notEqual(fruit, undefined, '`fruit` var should be defined')
+  t.equal(fruit, 'apple', '`fruit` var should be assigned the string "apple"')
+
+  t.equal(apple, fruit, '`apple` var should be same as `fruit` var')
   t.notEqual(apple, undefined, 'Apple var should be defined')
 })
 
+test('Test repeater function', (t) => {
+  t.plan(3)
+  var repeater = variables.repeater
+  var threeBeeps = repeater('beep', 3)
 
+  t.ok(Array.isArray(threeBeeps), '`repeater` function creates an array')
+  t.looseEqual(threeBeeps, ['beep', 'beep', 'beep'], 'should make an array of 3 "beeps"')
+
+  var noBeeps = repeater('beep', -1)
+  t.looseEqual(noBeeps, [], 'Should return empty array if N < 1')
+})
--- variables.js
+++ variables.js
@@ -1,8 +1,32 @@
+/** JS Fundamentals Review
+ *  Complete these exercises for a review on JavaScript fundamentals. Test your
+ *  work as you go by running `npm test` in a terminal open to this
+ *  project folder.
+ */
 
-// declare a variable `apple` and set it to the string 'apple'
-var apple = 'apple'
+// declare a variable `fruit` and assign it the string 'apple'
+var fruit = 'apple'
+
+// declare a variable `apple` and assign `fruit` to it
+var apple = fruit
+
+
+/* write a function `repeater` that takes a string `str` and a number `N` and returns an
+* array with `str` repeated `N` times
+* example: repeater('beep', 4) // => ['beep', 'beep', 'beep', 'beep']
+*/
+var repeater = function (str, N) {
+  return N <= 0
+    ? []
+    : [str].concat(repeater(str, N - 1))
+}
+
+/* write a function while */
+
 
 module.exports = {
-  apple: apple
+  fruit: fruit,
+  apple: apple,
+  repeater: repeater
 }
 
