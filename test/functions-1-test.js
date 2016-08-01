b8914ee883c521d6729fecf719ba9a0c171f77c2
jafow <jared.a.fowler@gmail.com>
2016-07-31 23:08:20 -0700

    break out functions challenges into own file and own test file

---
+++ test/functions-1-test.js
@@ -0,0 +1,14 @@
+const test = require('tape')
+const fn1 = require('./../functions-1')
+
+test('Test repeater function', (t) => {
+  t.plan(3)
+  var repeater = variables.repeater
+  var threeBeeps = repeater('beep', 3)
+
+  t.ok(Array.isArray(threeBeeps), '`repeater` function creates an array')
+  t.looseEqual(threeBeeps, ['beep', 'beep', 'beep'], 'should make an array of 3 "beeps"')
+
+  var noBeeps = repeater('beep', -1)
+  t.looseEqual(noBeeps, [], 'Should return empty array if N < 1')
+})
