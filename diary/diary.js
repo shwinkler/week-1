bcb4800627d4280109037b86197b672af5fb4d82
jafow <jared.a.fowler@gmail.com>
2016-08-01 17:00:35 -0700

    complete basic diary program

--- diary/diary.js
+++ diary/diary.js
@@ -1,13 +1,31 @@
 // Program for making a diary on the command line
 const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
+// store the path to diary file in a var for easy access
+const diaryPath = path.join(__dirname, 'diary.txt')
 
 // check to see if your diary.txt file is there. if not make it
-if (!fs.stat('./diary/diary.txt')) {
-  fs.writeFile('./diary/diary.txt', '', (err) => {
+fs.stat(diaryPath, (err, stat) => {
+  if (!stat.isFile()) makeEmptyDiaryFile(diaryPath)
+  if (err) throw err
+})
+
+// pipe command line into diary.txt file
+process.stdin.pipe(fs.createWriteStream(diaryPath))
+
+
+// helper functions
+function makeEmptyDiaryFile (filePath) {
+  fs.writeFile(filePath, '', 'utf8', (err) => {
     if (err) throw err
     console.log('created a file')
   })
 }
 
-// pipe command line into diary.txt file
-process.stdin.pipe(fs.createWriteStream('diary.txt'))
+/**extension: input a current date for each entry
+*  extension: create a new file with current date for each entry
+*  extension: create a directory
+*  extension: make a diary module!
+*/
