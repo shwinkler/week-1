efef3985c089eba1db56915c573590a88c3f86cf
jafow <jared.a.fowler@gmail.com>
2016-08-04 00:14:46 -0700

    add more closures and tests

--- closures/talk.js
+++ closures/talk.js
@@ -1,15 +1,37 @@
 
-// write a function that takes a string, console logs it and returns it
+// write a function `talk` that takes a string and returns it
 var talk = function (saying) {
   console.log(saying);
-  // return saying;
 }
 
+/* write a function `talk2` that takes any number of arguments and logs them to
+* the console.
+*/
+var talk2 = function (args) {
+  var ar = Array.from(arguments)
+  ar.forEach(a => { console.log(a) })
+}
 
-
-
+/** Write a function called `greeting` that 1 string parameter
+* and returns a function. The `greeting` function should use closure to store or
+* "remember" its string argument, and apply it to any argument passed to the
+* returned function. 
+* For example:
+*   var sayHello = greeting('hello')
+*   sayHello('alice')--> 'hello alice'
+*
+*   var sayGoodbye = greeting('goodbye')
+*   sayGoodbye('bob') --> 'goodbye bob'
+*/
+var greeting = function (str) {
+  return function (name) {
+    return `${str} ${name}`;
+  }
+}
 
 // do not alter the code below
 module.exports = {
-  talk: talk
+  talk: talk,
+  talk2: talk2,
+  greeting: greeting
 }
--- test/closures-test.js
+++ test/closures-test.js
@@ -1,11 +1,27 @@
 const test = require('tape')
 const spawn = require('tape-spawn')
-const talk = require('../closures/talk.js').talk
+const Talk = require('../closures/talk.js')
 
-test('tests talk ', (t) => {
-  t.plan(1)
-  talk('hi')
-  spawn.stdout.match(/hi/)
 
-  // t.equal(talk('hi'), 'hi', 'should return arg')
+test('Talk function', (t) => {
+  var st = spawn(t, 'printf "hi"')
+  var talk = Talk.talk
+  var hi = new RegExp(talk('hi'))
+  st.stdout.match(hi, 'talk should print arguments to the console')
+  st.end()
+})
+
+test('Talk2 function', (t) => {
+  var st = spawn(t, 'printf "every\nbody\ndance\"')
+  var talk2 = Talk.talk2
+  var hi = new RegExp(talk2('every', 'body', 'dance', 'now'))
+  st.stdout.match(hi, 'talk should print multiple arguments to the console')
+  st.end()
+})
+
+test('Greeting function', (t) => {
+  t.plan(2)
+  var greeting = Talk.greeting('hello')
+  t.equal(greeting('alice'), 'hello alice', 'should return "hello" + argument')
+  t.equal(greeting('bob'), 'hello bob')
 })
